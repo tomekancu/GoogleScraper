231c2913797806f19cf859fd815889286a5f6c59
Tomasz_Andrzej Ancukiewicz <tomasz_andrzej.ancukiewicz@student.put.poznan.pl>
2019-06-24 17:58:14 +0200

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Query(models.Model):
+    query_text = models.CharField(max_length=200)
+    user_ip = models.GenericIPAddressField()
+    asked_date = models.DateTimeField()
+    result_count = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.query_text} - {self.user_ip}"
+
+
+class Page(models.Model):
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+    nr = models.IntegerField(default=0)
+    url = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    destription = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.nr} - {self.title}"
