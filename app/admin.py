8a21ea6614051ff726685429670a5caa035d0259
Tomasz_Andrzej Ancukiewicz <tomasz_andrzej.ancukiewicz@student.put.poznan.pl>
2019-06-24 17:58:32 +0200

    admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Query, Page
+
+
+class QueryAdmin(admin.ModelAdmin):
+    list_display = ('query_text', 'user_ip', 'result_count', 'asked_date')
+    list_filter = ['asked_date', 'result_count']
+    search_fields = ['query_text']
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('query', 'nr', 'title')
+    list_filter = ['nr']
+    search_fields = ['title', 'destription', 'url']
+
+
+admin.site.register(Query, QueryAdmin)
+admin.site.register(Page, PageAdmin)
